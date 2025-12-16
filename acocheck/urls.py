6b84e6df17daf24120122d06dda5b9c41e1b4402
anurodh2100 <anurodh2100@gmail.com>
2025-12-16 18:47:13 +0530

    Fix corrupted urls.py causing

--- acocheck/urls.py
+++ acocheck/urls.py
@@ -3,9 +3,11 @@ from django.urls import path, include
 from django.shortcuts import redirect
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponse
 
 urlpatterns = [
-    path("", lambda request: redirect("dashboard:home")),
+    path("", lambda request: HttpResponse("AcoCheck is running ✅")),
+    # path("", lambda request: redirect("dashboard:home")),
     path("admin/", admin.site.urls),
     path("accounts/", include("apps.accounts.urls")),
     path("hostels/", include("apps.hostels.urls")),
